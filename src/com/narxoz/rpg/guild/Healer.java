79ab36328d0a80f793bf0f97a0981044627a1c24
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-05-05 01:23:54 +0500

    add Healer colleague for medical support

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -1,21 +1,82 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for wounds, potions, and recovery plans.
- */
 public class Healer extends GuildMember {
+    private int healingPotions = 30;
+    private int manaPotions = 25;
+    private int reviveScrolls = 5;
 
-    public Healer(String name, GuildMediator mediator) {
-        super(name, mediator);
-    }
-
-    public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
-        getMediator().dispatch(topic, this, payload);
+    public Healer(String name) {
+        super(name);
     }
 
     @Override
-    public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+    public void receive(String message, String topic, GuildMember sender) {
+        System.out.println("[HEALER " + name + "] Received message from " + sender.getName());
+
+        switch (topic) {
+            case "healing_request":
+                handleHealingRequest(message, sender);
+                break;
+            case "potion_request":
+                handlePotionRequest(message, sender);
+                break;
+            case "revive_request":
+                handleReviveRequest(sender);
+                break;
+            case "supply_response":
+                System.out.println("[HEALER " + name + "] Supply update: " + message);
+                break;
+            default:
+                System.out.println("[HEALER " + name + "] Ignoring topic: " + topic);
+        }
+    }
+
+    private void handleHealingRequest(String message, GuildMember sender) {
+        int healAmount = Integer.parseInt(message);
+        System.out.println("[HEALER " + name + "] Providing " + healAmount + " HP healing to " + sender.getName());
+        sender.send("Healed for " + healAmount, "healing_response");
+    }
+
+    private void handlePotionRequest(String message, GuildMember sender) {
+        String[] parts = message.split(":");
+        String potionType = parts[0];
+        int count = Integer.parseInt(parts[1]);
+
+        switch (potionType) {
+            case "healing":
+                if (healingPotions >= count) {
+                    healingPotions -= count;
+                    System.out.println("[HEALER " + name + "] Gave " + count + " healing potions to " + sender.getName());
+                    sender.send(count + " healing potions delivered", "potion_response");
+                } else {
+                    sender.send("Insufficient healing potions", "potion_response");
+                }
+                break;
+            case "mana":
+                if (manaPotions >= count) {
+                    manaPotions -= count;
+                    System.out.println("[HEALER " + name + "] Gave " + count + " mana potions to " + sender.getName());
+                    sender.send(count + " mana potions delivered", "potion_response");
+                } else {
+                    sender.send("Insufficient mana potions", "potion_response");
+                }
+                break;
+        }
+    }
+
+    private void handleReviveRequest(GuildMember sender) {
+        if (reviveScrolls > 0) {
+            reviveScrolls--;
+            System.out.println("[HEALER " + name + "] Used revive scroll on " + sender.getName());
+            sender.send("Revived", "revive_response");
+        } else {
+            System.out.println("[HEALER " + name + "] No revive scrolls left!");
+            sender.send("No revive scrolls available", "revive_response");
+        }
+    }
+
+    public void reportInventory() {
+        System.out.println("[HEALER " + name + "] Inventory: Healing=" + healingPotions +
+                ", Mana=" + manaPotions + ", Revive=" + reviveScrolls);
     }
 }
